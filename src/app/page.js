974763de1037c9a4be93ada4d6fda8b368f385a2
SiamIslamSagor <msiamislam12345@gmail.com>
2024-01-16 21:55:33 +0600

    added recent jobs data and start use fake json server

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-import { Button } from '@nextui-org/react';
-import React from 'react';
+import JobSection from "@/components/jobSection/JobSection";
+import { Button } from "@nextui-org/react";
+import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       This Is Home Page
       <Button> Remove</Button>
+      <JobSection />
     </div>
   );
 };
---
+++ src/components/jobSection/JobSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function JobSection() {
+  return <div className="">JobSection</div>;
+}
+
+export default JobSection;
