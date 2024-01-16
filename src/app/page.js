5f16573f2e9e0d59ed49cb734e48d29b47fec294
ferdous <papiaferdous43@gmail.com>
2024-01-16 23:55:25 +0600

    category section layout is complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import { Button } from '@nextui-org/react';
 import React from 'react';
-
+import Categories from '@/components/Categories';
 const HomePage = () => {
   return (
     <div>
       This Is Home Page
       <Button> Remove</Button>
+      <Categories></Categories>
     </div>
   );
 };
