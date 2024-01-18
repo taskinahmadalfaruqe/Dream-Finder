33ffa6f6fc37abc165a9e42dee59a9fb85b2a6cc
Mostaq27 <mostaq99muzahid@gmail.com>
2024-01-18 21:39:56 +0600

    [team] create contact page

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Categories from "@/components/Categories";
 
 const HomePage = () => {
   return (
-    <div className="p-20">
+    <div>
       <HomePageBanner />
       <Categories></Categories>
       <JobSection />
