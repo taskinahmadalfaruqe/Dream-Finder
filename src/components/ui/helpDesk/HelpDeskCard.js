7991a2ae99954fc5da2eb530fa39296df7aedff3
Mostaq27 <mostaq99muzahid@gmail.com>
2024-01-22 00:20:51 +0600

    upated helpdesk,contact page

--- src/components/ui/helpDesk/HelpDeskCard.js
+++ src/components/ui/helpDesk/HelpDeskCard.js
@@ -7,7 +7,7 @@ export default function HelpDeskCard() {
 
     return (
         <div>
-            <div className="block max-w-sm p-6  border border-gray-200 rounded-lg shadow   dark:border-gray-700 dark:hover:bg-gray-700">
+            <div className="block max-w-sm p-6  border border-primaryColor rounded-lg shadow   dark:border-gray-700 dark:hover:bg-gray-700">
 
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     GET IN TOUCH
