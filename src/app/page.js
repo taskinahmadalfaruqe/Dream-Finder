478dee6682ad52d2cb614fba8313474c069d628a
Mostaq27 <mostaq99muzahid@gmail.com>
2024-01-23 19:53:56 +0600

    resolve conflict

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,10 @@ import React from "react";
 import JobSection from "@/components/ui/jobSection/JobSection";
 import HelpDesk from "@/components/ui/helpDesk/helpDesk";
 import HomePageBanner from "@/components/ui/HomePageBanner/page";
-<<<<<<< HEAD
-import Categories from "@/components/Categories";
 import FeedBack from "@/components/ui/FeedBack/FeedBack";
 import Accordin from "@/components/ui/Accordion/Accordin";
-=======
 import Categories from "@/components/ui/Category/Categories";
 
->>>>>>> 3974aa58727c96bd88731d01f902830eb30bad81
 
 const HomePage = () => {
   return (
