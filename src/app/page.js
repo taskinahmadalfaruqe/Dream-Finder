ee343d4b9cc5ff508f785925c7441c03a150fd7a
ferdous <papiaferdous43@gmail.com>
2024-01-24 22:58:08 +0600

    complete and resolve issue subscription page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import React from "react";
-import JobSection from "@/components/ui/jobSection/JobSection";
-import HelpDesk from "@/components/ui/helpDesk/helpDesk";
-import HomePageBanner from "@/components/ui/HomePageBanner/page";
-import UpcomingEventModal from "@/components/ui/UpcomingEventModal/UpcomingEventModal";
-import FeedBack from "@/components/ui/FeedBack/FeedBack";
 import Accordin from "@/components/ui/Accordion/Accordin";
 import Categories from "@/components/ui/Category/Categories";
+import FeedBack from "@/components/ui/FeedBack/FeedBack";
+import HomePageBanner from "@/components/ui/HomePageBanner/page";
+import UpcomingEventModal from "@/components/ui/UpcomingEventModal/UpcomingEventModal";
+import HelpDesk from "@/components/ui/helpDesk/helpDesk";
+import JobSection from "@/components/ui/jobSection/JobSection";
+
 const HomePage = () => {
   return (
     <div>
