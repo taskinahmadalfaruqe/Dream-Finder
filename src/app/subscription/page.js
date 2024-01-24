e10e518fba13cfcc6f0716a7e8238a7eb91ef496
ferdous <papiaferdous43@gmail.com>
2024-01-24 20:34:49 +0600

    some changes

--- src/app/subscription/page.js
+++ src/app/subscription/page.js
@@ -9,7 +9,6 @@ const SubscribePage = () => {
         bg-gradient-to-r from-emerald-500 from-10% via-sky-500 via-30% to-indigo-500 to-90%'>
             <h2 className='container text-center text-4xl font-bold'>For More Facilities Please <span className='text-pink-600 text-5xl'>Subscribe </span> To Us!</h2>
             <Subscribe></Subscribe>
-
         </div>
     );
 };
