95330d613bc838f4d485bcea8993a3d8b1fb00e5
HalimaTusSadiya <halimasaadiya66@gmail.com>
2024-01-28 20:25:18 +0600

    added dynamic page for each job post

---
+++ src/app/Find-Jobs/[id]/page.js
@@ -0,0 +1,12 @@
+import JobDetails from '@/components/ui/jobDetails/JobDetails';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <JobDetails />
+        </div>
+    );
+};
+
+export default page;
