4885377675145d05849352acb48e5efac5041a66
HalimaTusSadiya <halimasaadiya66@gmail.com>
2024-01-28 20:33:52 +0600

    trying to resolve dynamic page issue

--- src/app/Find-Jobs/[id]/page.js
+++ src/app/Find-Jobs/[id]/page.js
@@ -1,10 +1,10 @@
-import JobDetails from '@/components/ui/jobDetails/JobDetails';
+// import JobDetails from '@/components/ui/jobDetails/JobDetails';
 import React from 'react';
 
 const page = () => {
     return (
-        <div>
-            <JobDetails />
+        <div className='container'>
+            <h1 className='text-5xl'>Details Page</h1>
         </div>
     );
 };
