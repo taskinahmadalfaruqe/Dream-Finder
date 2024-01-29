14513cb9a95716b7f9dad02e70545852ceb0a6c4
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-01-29 16:29:56 +0600

    Add Dynamic Page And Fix Dashboard Layout

--- src/app/(dashboards)/layout.js
+++ src/app/(dashboards)/layout.js
@@ -1,7 +1,12 @@
+import Footer from "@/components/shared/Footer";
+import NextNavbar from "@/components/shared/NextNav";
+
 const DashboardLayout = ({ children }) => {
     return (
         <div>
+            <NextNavbar></NextNavbar>
             { children }
+            <Footer></Footer>
         </div>
     );
 };
