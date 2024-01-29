a61b76f683c6faf0069a75e7fd5c73a2e4d88f9e
HalimaTusSadiya <halimasaadiya66@gmail.com>
2024-01-29 10:20:06 +0600

    trying to resolve dynamic page issue after deploying

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    swcMinify: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
   },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '*'
-        },
-      ],
-    },
-  }
-
-module.exports = nextConfig
+};
+module.exports = nextConfig;
