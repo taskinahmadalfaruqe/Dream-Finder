ef1b1a136eea76952e5eec094fc2b5665b1cc6a5
ferdous <papiaferdous43@gmail.com>
2024-02-07 19:34:47 +0600

    complete marge

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,7 +3,7 @@ const LoadingPage = () => {
     <div className="min-h-[70vh] flex items-center justify-center">
       <span className="loader"></span>
     </div>
-  );
+);
 };
 
 export default LoadingPage;
