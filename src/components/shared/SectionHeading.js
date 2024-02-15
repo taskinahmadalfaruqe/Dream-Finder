30bfea2e72aec34cfd7ae6abc5bd885540706f55
SiamIslamSagor <msiamislam12345@gmail.com>
2024-02-16 01:02:13 +0600

    [feat](data-formatting) : complete job post from data accessing and data post format.

--- src/components/shared/SectionHeading.js
+++ src/components/shared/SectionHeading.js
@@ -3,9 +3,13 @@ import PropTypes from "prop-types";
 
 const SectionHeading = ({ heading, subHeading }) => {
   return (
-    <div className="text-center space-y-2 pb-10">
-      <h2 className="text-3xl font-bold text-center text-darkColor dark:text-whiteColor ">{heading}</h2>
-      <p className="text-sm text-center text-secondaryColor dark:text-lightWhiteColor">{subHeading}</p>
+    <div className="text-center space-y-2 pb-5">
+      <h2 className="text-3xl font-bold text-center text-darkColor dark:text-whiteColor ">
+        {heading}
+      </h2>
+      <p className="text-sm text-center text-secondaryColor dark:text-lightWhiteColor">
+        {subHeading}
+      </p>
     </div>
   );
 };
