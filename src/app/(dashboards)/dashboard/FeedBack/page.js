0d3c7cf1ac219b66e07968986001204d6da1ad37
Mostaq27 <mostaq99muzahid@gmail.com>
2024-02-25 01:13:16 +0600

    [contact] fixed bug and  posted contact form data to the database

---
+++ src/app/(dashboards)/dashboard/FeedBack/page.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import FeedBackForm from './../../../../components/ui/FeedbackForm/FeedBackForm';
+
+const FeedBackPage = () => {
+    return (
+        <div>
+            <h2 className='text-3xl font-bold text-center text-primaryColor mb-8 my-10'>FeedBack</h2>
+            <FeedBackForm></FeedBackForm>
+        </div>
+    );
+};
+
+export default FeedBackPage;
